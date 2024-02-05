3861f96a3830c101f3bb7fd33497010fdabbfe8f
unknown <biswasmahmuda5@gmail.com>
2024-02-04 19:54:46 -0800

    add es6 fetures

---
+++ es6.js
@@ -0,0 +1,11 @@
+const numbers = [2, 34, 56, 78, 34, 53, 34];
+const student = {
+  name: "deem",
+  age: 23,
+  drams: [12, 32, 23, 43, 33],
+};
+const about = `
+${student.age}
+${numbers.length}
+`;
+console.log(about);
---
+++ fundamental.js
@@ -0,0 +1,22 @@
+// how to declear a variable using var let and const
+const fatherName = "mukul";
+let sesone = "winter";
+sesone = "autum";
+
+// 6 basic condition
+// multipule conditon
+//  array decleration
+// index
+// length push pop
+
+// function
+
+function multipule(num1, num2) {
+  const multi = num1 * num2;
+  return multi;
+}
+const result = multipule(2, 4);
+console.log(result);
+
+// Object
+// 3 way to access to property by name;
